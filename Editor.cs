38189a58b49573154f6c7ca1623c14ac6acd7cad
Isabelle Ricciolini <87038843+isaricciolini@users.noreply.github.com>
2023-04-04 18:42:28 -0300

    Viewer

--- Editor.cs
+++ Editor.cs
@@ -23,7 +23,7 @@ namespace EditorHTML{
             
             Console.WriteLine("----------");
             Console.WriteLine(" Deseja salvar o arquivo?");
-
+            Viewer.Show(file.ToString());
         }
     }
 }
--- Menu.cs
+++ Menu.cs
@@ -11,7 +11,7 @@ namespace EditorHTML{
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
-
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen(){
@@ -39,20 +39,22 @@ namespace EditorHTML{
         }
     
         public static void WriteOptions(){
-            Console.SetCursorPosition(3, 2);
+            Console.SetCursorPosition(3, 1);
             Console.WriteLine("Editor HTML");
-            Console.SetCursorPosition(3, 3);
+            Console.SetCursorPosition(3, 2);
             Console.WriteLine("==========");
-            Console.SetCursorPosition(3, 4);
+            Console.SetCursorPosition(3, 3);
             Console.WriteLine("Selecione uma opção abaixo:");
-            Console.SetCursorPosition(3, 6);
+            Console.SetCursorPosition(3, 5);
             Console.WriteLine("1 - Novo arquivo");
-            Console.SetCursorPosition(3, 7);
+            Console.SetCursorPosition(3, 6);
             Console.WriteLine("2 - Abrir");
-            Console.SetCursorPosition(3, 9);
+            Console.SetCursorPosition(3, 7);
             Console.WriteLine("0 - Sair");
-            Console.SetCursorPosition(3, 10);
-            Console.Write("Opção: ");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("Opção: ");
+            Console.SetCursorPosition(3, 9);
+            Console.Write(" ");
         }
     
         public static void HandleMenuOption(short option){
